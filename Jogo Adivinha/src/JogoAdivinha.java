9dfb060785f9e7882c0311bce1cf1e20d9a1effe
Oluasy <62860960+Oluasy@users.noreply.github.com>
2024-10-07 18:15:46 -0300

    Add files via upload

---
+++ Jogo Adivinha/src/JogoAdivinha.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdivinha {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numberTyped = 0;
+        int trys = 0;
+        int randomNumber = random.nextInt(100);
+        System.out.println("Tente adivinhar o número aleatório entre 0 a 100!");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite um número entre 0 a 100: ");
+            numberTyped = scanner.nextInt();
+
+            if (numberTyped == randomNumber) {
+                System.out.println("Parabéns, você acertou o número em " + (trys + 1) + " tentativas!");
+                break;
+            } else if (numberTyped > randomNumber) {
+                System.out.println("O número digitado é maior que o número gerado.");
+                System.out.println();
+                trys++;
+            } else if (numberTyped < randomNumber) {
+                System.out.println("O número digitado é menor que o número gerado.");
+                System.out.println();
+                trys++;
+            }
+        }
+
+        if (trys == 5) {
+            System.out.println("Você não conseguiu acertar o número gerado em 5 tentativas, o número era: " + randomNumber);
+        }
+    }
+}
